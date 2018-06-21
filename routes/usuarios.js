596c3e664841bc032b92a59af7865542f9b92d78
jhosexd0 <39969899+jhosexd0@users.noreply.github.com>
2018-06-21 14:04:21 -0500

    Agregando las rutas

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const router = express.Router();
+const userModel = require('../models/usuarios');
 
-const usuarioModel= require('../models/usuarios');
-
-router.get('/',usuarioModel.find);
+router.get('/',userModel.find);
+router.get('/:id',userModel.findOne);
+router.post('/',userModel.create);
+router.put('/:id',userModel.update);
+router.delete('/:id',userModel.delete);
 
 module.exports = router;
